d15b60215176693b07bc285080cdd4836984a6a4
Ricardo Ramírez <rickyrb@gmail.com>
2019-03-28 19:55:20 -0500

    * Se elimina un espacio...

--- src/main/java/com/sophossolutions/pocga/api/WebConfig.java
+++ src/main/java/com/sophossolutions/pocga/api/WebConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
- 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**");
