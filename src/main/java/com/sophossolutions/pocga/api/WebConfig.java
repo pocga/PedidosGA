ddce97079e0fbe5b5eae71d2a6f0a79bfbd39d79
SBMEDLAPFABRR1\RICARDO.RAMIREZ <rickyrb@gmail.com>
2019-03-29 10:16:33 -0500

    * Habilita CORS para toda la API, y para TODOS los métodos

--- src/main/java/com/sophossolutions/pocga/api/WebConfig.java
+++ src/main/java/com/sophossolutions/pocga/api/WebConfig.java
@@ -15,7 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry
+			.addMapping("/**")
+			.allowedMethods("*")
+		;
     }
 
 }
