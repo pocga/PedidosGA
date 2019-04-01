0dc1cb50ea75401e3faa8087c86baabcdaccc624
SBMEDLAPFABRR1\RICARDO.RAMIREZ <rickyrb@gmail.com>
2019-04-01 16:46:25 -0500

    * Ajuste de la ruta de la API en producción

--- src/main/java/com/sophossolutions/pocga/Application.java
+++ src/main/java/com/sophossolutions/pocga/Application.java
@@ -15,7 +15,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		LOGGER.info("¡HOLA, SOY UNA NUEVA VERSIÓN!");
+		LOGGER.info("¡HOLA, SOY OTRO DESPLIEGUE AUTOMÁTICO!");
 	}
 
 }
