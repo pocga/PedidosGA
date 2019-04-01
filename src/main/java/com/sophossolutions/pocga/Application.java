e89b7d8708bbcca0ad7816ac77860fb3f50e1459
SBMEDLAPFABRR1\RICARDO.RAMIREZ <rickyrb@gmail.com>
2019-04-01 16:10:04 -0500

    * Log para verificar despligue continuo

--- src/main/java/com/sophossolutions/pocga/Application.java
+++ src/main/java/com/sophossolutions/pocga/Application.java
@@ -1,5 +1,7 @@
 package com.sophossolutions.pocga;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,8 +10,12 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class Application {
 
+	/** Log de eventos */
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		LOGGER.info("¡HOLA, SOY UNA NUEVA VERSIÓN!");
 	}
 
 }
