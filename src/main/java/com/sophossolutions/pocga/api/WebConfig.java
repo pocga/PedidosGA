cdb03a047a48e147022a6f16141851ef2b1c477d
daniel_rodriguez <daniel.rodriguez@sophossolutions.com>
2019-04-05 21:22:28 -0500

    Moviendo el WebConfig al paquete api

--- src/main/java/com/sophossolutions/pocga/api/WebConfig.java
+++ src/main/java/com/sophossolutions/pocga/api/WebConfig.java
@@ -1,4 +1,4 @@
-package com.sophossolutions.pocga.config;
+package com.sophossolutions.pocga.api;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
